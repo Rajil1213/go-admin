565787aca80ac590456110ee690ba64b4cd21307
Rajil Bajracharya <imrb1213@gmail.com>
2022-10-04 16:52:11 +0545

    Add pagination for users

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"math"
 	"strconv"
 
 	"github.com/Rajil1213/go-admin/database"
@@ -11,11 +12,25 @@ import (
 const DefaultPassword = "changeme"
 
 func AllUsers(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page", "1"))
+	limit := 5
+	offset := (page - 1) * limit
+
+	var total int64
+	database.DB.Model(&models.User{}).Count(&total)
+
 	var users []models.User
 
-	database.DB.Preload("Role").Find(&users)
+	database.DB.Preload("Role").Offset(offset).Limit(limit).Find(&users)
 
-	return c.JSON(users)
+	return c.JSON(fiber.Map{
+		"data": users,
+		"meta": fiber.Map{
+			"total":     total,
+			"page":      page,
+			"last_page": math.Ceil(float64(total) / float64(limit)),
+		},
+	})
 }
 
 func CreateUser(c *fiber.Ctx) error {
