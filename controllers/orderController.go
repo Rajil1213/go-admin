75337fee8a2c8ae24a5381b6eb6696d9bc371fbb
Rajil Bajracharya <imrb1213@gmail.com>
2022-10-04 22:35:18 +0545

    Add functinality to export orders to csv

--- controllers/orderController.go
+++ controllers/orderController.go
@@ -1,6 +1,8 @@
 package controllers
 
 import (
+	"encoding/csv"
+	"os"
 	"strconv"
 
 	"github.com/Rajil1213/go-admin/database"
@@ -12,4 +14,65 @@ func AllOrders(c *fiber.Ctx) error {
 	page, _ := strconv.Atoi(c.Query("page", "1"))
 
 	return c.JSON(models.Paginate(database.DB, &models.Order{}, page))
+}
+
+func Export(c *fiber.Ctx) error {
+	filePath := "./csv/orders.csv"
+
+	if err := CreateFile(filePath); err != nil {
+		return err
+	}
+
+	return c.Download(filePath)
+}
+
+func CreateFile(filePath string) error {
+	file, err := os.Create(filePath)
+	defer file.Close()
+
+	if err != nil {
+		return err
+	}
+
+	writer := csv.NewWriter(file)
+	defer writer.Flush()
+
+	var orders []models.Order
+
+	database.DB.Preload("OrderItems").Find(&orders)
+
+	writer.Write([]string{
+		"ID", "Name", "Email", "Product Title", "Price", "Quantity",
+	})
+
+	for _, order := range orders {
+		data := []string{
+			strconv.Itoa(int(order.Id)),
+			order.FirstName + " " + order.LastName,
+			order.Email,
+			"",
+			"",
+			"",
+		}
+
+		if err := writer.Write(data); err != nil {
+			return err
+		}
+		for _, orderItem := range order.OrderItems {
+			data := []string{
+				"",
+				"",
+				"",
+				orderItem.ProductTitle,
+				strconv.Itoa(int(orderItem.Price)),
+				strconv.Itoa(int(orderItem.Quantity)),
+			}
+
+			if err := writer.Write(data); err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
 }
