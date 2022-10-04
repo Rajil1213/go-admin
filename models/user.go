d50532d581bb351be957a14c37d177fd8ea393b1
Rajil Bajracharya <imrb1213@gmail.com>
2022-10-04 17:30:52 +0545

    Implement entity interface methods

--- models/user.go
+++ models/user.go
@@ -2,6 +2,7 @@ package models
 
 import (
 	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
 
 type User struct {
@@ -21,4 +22,16 @@ func (user *User) SetPassword(password string) {
 
 func (user *User) CheckPassword(password string) error {
 	return bcrypt.CompareHashAndPassword(user.Password, []byte(password))
+}
+
+func (user *User) Count(db *gorm.DB) int64 {
+	var total int64
+	db.Model(User{}).Count(&total)
+	return total
+}
+
+func (user *User) Take(db *gorm.DB, offset int, limit int) interface{} {
+	var users []User
+	db.Preload("Role").Offset(offset).Limit(limit).Find(&users)
+	return users
 }
