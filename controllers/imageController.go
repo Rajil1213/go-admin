3bf025df53a8f1219ac7d7e37b1f453576f0ba02
Rajil Bajracharya <imrb1213@gmail.com>
2022-10-04 20:36:59 +0545

    Add function to upload images

---
+++ controllers/imageController.go
@@ -0,0 +1,27 @@
+package controllers
+
+import "github.com/gofiber/fiber/v2"
+
+func Upload(c *fiber.Ctx) error {
+	form, err := c.MultipartForm()
+
+	if err != nil {
+		return err
+	}
+
+	files := form.File["image"]
+	var filename string
+
+	for _, file := range files {
+		filename = file.Filename
+
+		if err := c.SaveFile(file, "./uploads/"+filename); err != nil {
+			return err
+		}
+	}
+
+	return c.JSON(fiber.Map{
+		"url": "http://localhost:3000/api/uploads/" + filename,
+	})
+
+}
