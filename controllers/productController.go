627d658c2cde0618cdebca48c6726198a10acfe0
Rajil Bajracharya <imrb1213@gmail.com>
2022-10-04 17:29:01 +0545

    Create controller for the product model

---
+++ controllers/productController.go
@@ -0,0 +1,70 @@
+package controllers
+
+import (
+	"strconv"
+
+	"github.com/Rajil1213/go-admin/database"
+	"github.com/Rajil1213/go-admin/models"
+	"github.com/gofiber/fiber/v2"
+)
+
+func AllProducts(c *fiber.Ctx) error {
+	page, _ := strconv.Atoi(c.Query("page", "1"))
+
+	return c.JSON(models.Paginate(database.DB, &models.Product{}, page))
+}
+
+func CreateProduct(c *fiber.Ctx) error {
+	/* This function is for when a registered user creates another user */
+	var product models.Product
+
+	if err := c.BodyParser(&product); err != nil {
+		return err
+	}
+
+	database.DB.Create(&product)
+
+	return c.JSON(product)
+}
+
+func GetProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	product := models.Product{
+		Id: uint(id),
+	}
+
+	database.DB.Find(&product)
+
+	return c.JSON(product)
+}
+
+func UpdateProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	product := models.Product{
+		Id: uint(id),
+	}
+
+	if err := c.BodyParser(&product); err != nil {
+		return err
+	}
+
+	database.DB.Model(&product).Updates(product)
+
+	return c.JSON(product)
+}
+
+func DeleteProduct(c *fiber.Ctx) error {
+	id, _ := strconv.Atoi(c.Params("id"))
+
+	product := models.Product{
+		Id: uint(id),
+	}
+
+	database.DB.Delete(&product)
+
+	return c.JSON(fiber.Map{
+		"message": "Success",
+	})
+}
